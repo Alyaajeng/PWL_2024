4eae6ac7360785af4a499b800ed7c0199d4c6175
Alyaajeng <alyaajeng29@gmail.com>
2025-02-28 11:01:18 +0700

    Pembuatan file migrasi dengan relasi

---
+++ POS/database/migrations/2025_02_28_033617_create_t_penjualan_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_penjualan', function (Blueprint $table) {
+            $table->id('penjualan_id'); // Primary Key
+            $table->date('tanggal');
+            $table->decimal('total_harga', 10, 2);
+            $table->unsignedBigInteger('user_id'); // Foreign Key
+            $table->timestamps();
+
+            // Definisi Foreign Key
+            $table->foreign('user_id')->references('user_id')->on('m_user')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_penjualan');
+    }
+};
