5f5ec44a8cf1f361ef2bef003631d5fbe86ec127
Alyaajeng <alyaajeng29@gmail.com>
2025-02-28 13:55:25 +0700

    Query Builder

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\LevelController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KategoriController;
 
 /*
 |--------------------------------------------------------------------------
@@ -42,3 +43,12 @@ Route::get('/sales', [SalesController::class, 'index']);
     });
     
     Route::get('/level', [LevelController:: class, 'index']);
+
+    
+    
+    // Query Builder
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::get('/level', [LevelController::class, 'index']);
+    Route :: get('/kategori', [KategoriController :: class, 'index']);
