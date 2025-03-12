a9ab31d50565eb3ebb26aa92172eb0cb344da798
Alyaajeng <alyaajeng29@gmail.com>
2025-03-12 21:04:59 +0700

    JS 4_Praktikum 2.6 Create, Read, Update, Delete (CRUD)

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -3,52 +3,38 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SalesController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// menambahkan route halaman home
 Route::get('/', [HomeController::class, 'index']);
 
-//menambahkan route halaman products 
+// Route halaman kategori produk
 Route::prefix('category')->group(function () {
     Route::get('/{category}', [ProductController::class, 'category']);
 });
 
-// menambahkan route halaman user 
+// Route halaman user
+Route::get('/user', [UserController::class, 'index']);
 Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
 
-// menambahkan route halaman penjualan
+// Route halaman penjualan
 Route::get('/sales', [SalesController::class, 'index']);
 
-// DB FACADE
-    Route::get('/', function () {
-        return view('welcome');
-    });
-    
-    Route::get('/level', [LevelController:: class, 'index']);
+// Route database facade
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 
-    
-    Route::get('/', function () {
-        return view('welcome');
-    });
-    
-    Route::get('/level', [LevelController::class, 'index']);
-    Route :: get('/kategori', [KategoriController :: class, 'index']);
-    Route :: get('/user', [UserController :: class, 'index']);
+// **CRUD User**
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']); // Perbaikan
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
