042566ac544e9b479a8e59797cc78dbfa32474d7
Alyaajeng <alyaajeng29@gmail.com>
2025-04-16 10:20:17 +0700

    JS5 - Praktikum3

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -15,42 +15,30 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::get('/', [HomeController::class, 'index']);
-
-// Route halaman kategori produk
-Route::prefix('category')->group(function () {
-    Route::get('/{category}', [ProductController::class, 'category']);
-});
-
-// Route halaman user
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
-
-// Route halaman penjualan
-Route::get('/sales', [SalesController::class, 'index']);
-
-// Route database facade
-Route::get('/level', [LevelController::class, 'index']);
-
-// **CRUD User**
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']); // Perbaikan
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-
-Route::get('/', function () {
-    return view('welcome'); // Sesuaikan dengan file view yang ada
-});
-
-Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
-Route::post('/kategori', [KategoriController::class, 'store']);
-
-Route::get('/kategori/{id}/edit', [KategoriController::class, 'edit'])->name('kategori.edit');
-Route::post('/kategori/{id}', [KategoriController::class, 'update'])->name('kategori.update');
-
-Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
-
-Route::delete('/kategori/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
-
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    // Menampilkan halaman awal user
+    Route::get('/', [UserController::class, 'index']);
+
+    // Menampilkan data user dalam bentuk JSON untuk DataTables
+    Route::post('/list', [UserController::class, 'list']);
+
+    // Menampilkan halaman form tambah user
+    Route::get('/create', [UserController::class, 'create']);
+
+    // Menyimpan data user baru
+    Route::post('/', [UserController::class, 'store']);
+    
+    // Menampilkan detail user
+    Route::get('/{id}', [UserController::class, 'show']);
+
+    // Menampilkan halaman form edit user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);
+
+    // Menyimpan perubahan data user
+    Route::put('/{id}', [UserController::class, 'update']);
+
+    // Menghapus data user
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
