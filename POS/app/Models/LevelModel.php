3f70bf0ca3f46acd00d6e7d09ffc2a7dcb64ac43
Alyaajeng <alyaajeng29@gmail.com>
2025-04-18 12:55:42 +0700

    JS6 - TUGAS AJAX m_supplier

--- POS/app/Models/LevelModel.php
+++ POS/app/Models/LevelModel.php
@@ -4,18 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use App\Models\UserModel;
 
 class LevelModel extends Model
 {
     use HasFactory;
-
     protected $table = 'm_level';
-    protected $primarykey = 'level_id';
+    protected $primaryKey = 'level_id';
 
-    protected $fillable = ['level_kode', 'level_nama'];
-
-    public function users()
+    public function user(): HasMany
     {
         return $this->hasMany(UserModel::class, 'level_id', 'level_id');
     }
+
+    protected $fillable = [
+        'level_id',
+        'level_kode',
+        'level_nama',
+    ];
 }
